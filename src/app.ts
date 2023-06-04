9d35d29e22c98934efedd96724adf1a69fc90557
mjrgeorge <mjrgeorge@gmail.com>
2023-06-04 12:08:56 +0600

    error handle completed

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-import usersRouter from './modules/users/users.route'
-
+import express, { Application } from 'express'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -9,11 +9,16 @@ app.use(cors())
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/api/v1/users', usersRouter)
 
-//Testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working Successfully')
-})
+app.use('/api/v1/users/', usersRouter)
+
+// //Testing
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   // throw new ApiError(400, 'Ore Baba Error ')
+//   // next('Ore Baba Error') // Error
+// })
+
+//global error handler
+app.use(globalErrorHandler)
 
 export default app
